4fccc68289b50b1f18e260237e1b04d4b89a5d5b
yazanAlhalabi <yazanalhalabi1@gmail.com>
2019-07-08 17:13:32 +0300

    import font fix

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,7 @@
+import React from "react"
 import { Link } from "gatsby"
 import classNames from "classnames"
 import PropTypes from "prop-types"
-import React from "react"
 import "./styles.scss"
 
 const Header = ({ siteTitle, isGreen }) => {
