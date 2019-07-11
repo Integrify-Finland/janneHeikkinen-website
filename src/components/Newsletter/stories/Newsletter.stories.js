d0b8d7c65728b03126c082be1af3c0cfb1a1c82a
yazanAlhalabi <yazanalhalabi1@gmail.com>
2019-07-11 13:52:44 +0300

    fix animation

--- src/components/Newsletter/stories/Newsletter.stories.js
+++ src/components/Newsletter/stories/Newsletter.stories.js
@@ -4,8 +4,17 @@ import { storiesOf } from "@storybook/react"
 import { jsxDecorator } from "storybook-addon-jsx"
 
 import Newsletter from ".."
+
+const styles = {
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  height: "100vh",
+}
 storiesOf("Newsletter", module)
-    .addDecorator(jsxDecorator)
-    .add("newsletter", () => 
-    <Newsletter />
-    )
+  .addDecorator(jsxDecorator)
+  .add("newsletter", () => (
+    <div style={styles}>
+      <Newsletter />
+    </div>
+  ))
