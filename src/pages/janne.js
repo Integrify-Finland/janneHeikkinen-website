86f5dc44765b9c4c33b752bccaa3b6ec64e987fb
yazanAlhalabi <yazanalhalabi1@gmail.com>
2019-07-13 18:32:34 +0300

    fixed text-block

--- src/pages/janne.js
+++ src/pages/janne.js
@@ -4,13 +4,36 @@ import { graphql } from "gatsby"
 import Layout from "../components/Layout"
 import SEO from "../components/SEO"
 import Resume from "../components/Resume"
+import AboutTextBlock from "../components/About-text-block"
+import Section from "../components/Section"
+
+import imageFile from "../images/gatsby-icon.png"
+import astro from "../images/gatsby-astronaut.png"
 
 const Janne = ({ data }) => {
-  const { resume } = data
-
+  const { resume, DetailsAboutMe } = data
+  const revertedDetails = DetailsAboutMe.edges.reverse()
   return (
     <Layout>
       <SEO title="Janne" />
+      <Section>
+        {revertedDetails.map(({ node }, i) => {
+          return i % 2 !== 0 ? (
+            <AboutTextBlock
+              title={node.title}
+              text={node.description.description}
+              icon={imageFile}
+            />
+          ) : (
+            <AboutTextBlock
+              title={node.title}
+              text={node.description.description}
+              isLeft
+              icon={astro}
+            />
+          )
+        })}
+      </Section>
       <Resume
         opinnot={resume.opinnot}
         varusmies={resume.varusmiespalvelus}
