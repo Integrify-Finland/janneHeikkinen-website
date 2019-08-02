89da27205b6671516cc8fff7e36a5ae09523f70e
André Vollrath <andrevollrath@Andres-MacBook-Pro.local>
2019-08-02 12:19:59 +0300

    added About header

--- src/components/Header/__stories__/Header.stories.js
+++ src/components/Header/__stories__/Header.stories.js
@@ -7,4 +7,9 @@ import Header from ".."
 
 storiesOf("Header", module)
   .addDecorator(jsxDecorator)
-  .add("with Subtext", () => <Header Subtext="Kansanedustaja, yhteiskuntatieteiden maisteri ja intohimoinen perhokalastaja."/>)
+  .add("Etusivu Header", () => <Header Subtext="Kansanedustaja, yhteiskuntatieteiden maisteri ja intohimoinen perhokalastaja."/>)
+
+  .add("About Header", () => <Header isAbout={true} Kotipaikka="Heinäpää, Oulu" Syntynyt="1990 Oulussa" Perhe="Avovaimo Mira sekä äiti Sirpa, isä Kari, sisko Sanni ja veli Matti sekä pohjanpystykorva Sissi." Sotilasarvo_aselinja="jääkäri, sissi" Ammatti="Viestinnän suunnittelija" Harrastukset="Salibandy, sulkapallo, kalastus, tennis" Koulutus="Yhteiskuntatieteiden maisteri Jyväskylän yliopistosta" Luottamustehtava="Kansanedustaja, kaupunginvaltuutettu"/>)
+
+
+
