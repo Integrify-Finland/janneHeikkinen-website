81833046f887a887948edc51743db31ad21834ba
André Vollrath <andrevollrath@Andres-MacBook-Pro.local>
2019-08-08 10:40:44 +0300

    worked on gatsby config and social Media markdown

---
+++ src/components/SocialMedia/index.js
@@ -0,0 +1,52 @@
+
+import React from "react"
+import "./styles.scss"
+import { Link, graphql } from "gatsby";
+import get from "lodash/get";
+
+class SocialMedia extends React.Component {
+  render() {
+    
+    const posts = get(this, "props.data.allMarkdownRemark.edges");
+
+    return (
+      <div>
+  
+        {posts.map(({ node }) => {
+          const title = get(node, "frontmatter.title") || node.fields.slug;
+          return (
+            <article key={node.fields.slug}>
+              <h1>
+                <Link style={{ boxShadow: "none" }} to={node.fields.slug}>
+                  {title}
+                </Link>
+              </h1>
+              <div dangerouslySetInnerHTML={{ __html: node.html }} />
+            </article>
+          );
+        })}
+      </div>
+    );
+  }
+}
+
+export default SocialMedia;
+
+export const SocialMediaQuery = graphql`
+  query {
+    
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      edges {
+        node {
+          fields {
+            slug
+          }
+          html
+          frontmatter {
+            title
+          }
+        }
+      }
+    }
+  }
+`;
---
+++ src/components/SocialMedia/instagram.md
@@ -0,0 +1,3 @@
+Embedded Instagram post!
+
+https://www.instagram.com/p/BohahKdhVTO/
