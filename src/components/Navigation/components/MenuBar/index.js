5b75e95d9efb06ec5f095a7ec6878ce839de599e
yazanAlhalabi <yazanalhalabi1@gmail.com>
2019-08-13 12:26:48 +0300

    renaming

--- src/components/Navigation/components/MenuBar/index.js
+++ src/components/Navigation/components/MenuBar/index.js
@@ -58,7 +58,7 @@ const MenuBar = ({ toggleMenu }) => (
         </Link>
 
         <Link
-          to="/yhteystiedot"
+          to="/yhteys"
           className="navbar__nav-link"
           activeClassName="navbar__nav-link--active"
         >
