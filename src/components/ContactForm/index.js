1d29472ed3c01405a1771579a13f3a97b2a0bc74
Julia Shumeyko <funkydinosaur@Julias-MacBook-Air.local>
2019-08-22 16:39:13 +0300

    added feedback on input error

--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 import { navigateTo } from "gatsby-link"
 import NetlifyForm from "react-netlify-form"
 
@@ -6,6 +6,72 @@ import Button from "../Button"
 import "./styles.scss"
 
 const ContactForm = () => {
+
+  const [name, setName] = useState();
+  const [email, setEmail] = useState();
+  const [message, setMessage] = useState();
+  const [nameError, setNameError] = useState();
+  const [emailError, setEmailError] = useState();
+  const [messageError, setMessageError] = useState();
+  const [disable, setDisable] = useState(true);
+
+  const namePattern = /^[à-ža-zÀ-ŽA-Z-.' ]{2,40}$/
+  const emailPattern = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  const messagePattern = /^.{2,450}$/
+  
+
+  const handleChange = event => { 
+
+    if(event.target.name === "name") { 
+      setName(event.target.value);
+    } 
+    if(event.target.name === "email") { 
+      setEmail(event.target.value)
+    } 
+    if(event.target.name === "message") { 
+      setMessage(event.target.value)
+    } 
+
+    if (namePattern.test(name) && emailPattern.test(email) && messagePattern.test(message)) {
+      setDisable(false);
+    } else {
+      setDisable(true);
+    }
+    
+  }
+
+  const validateName = () => {
+
+    if (namePattern.test(name) !== true) {
+      setNameError('Nimen tulee olla vähintään 2 kirjaimen mittainen ja siinä ei saa olla numeroita')
+    } else {
+      setNameError(null)
+    }
+
+  }
+
+  const validateEmail = () => {
+
+    if (emailPattern.test(email) !== true) {
+      setEmailError('Väärä sähköposti')
+    } else {
+      setEmailError(null)
+    }
+
+  }
+
+  const validateMessage = () => {
+
+    if (messagePattern.test(message) !== true) {
+      setMessageError('Viestin tulee olla 2-450 kirjaimen mittainen')
+    } else {
+      setMessageError(null)
+    }
+
+  }
+  
+
+
   const renderDisabledForm = msg => {
     return (
       <>
@@ -71,9 +137,13 @@ const ContactForm = () => {
                       type="text"
                       name="name"
                       className="contact-form__input"
+                      onChange={handleChange}
+                      onBlur={validateName}
+                      value={name}
                     />
                   </label>
                 </p>
+                <div className='invalid-feedback'>{nameError}</div>
                 <p>
                   <label>
                     Sähköposti (pakollinen)
@@ -82,9 +152,13 @@ const ContactForm = () => {
                       type="email"
                       name="email"
                       className="contact-form__input"
+                      onChange={handleChange}
+                      onBlur={validateEmail}
+                      value={email}
                     />
                   </label>
                 </p>
+                <div className='invalid-feedback'>{emailError}</div>
                 <p>
                   <label>
                     Viestisi
@@ -93,12 +167,16 @@ const ContactForm = () => {
                       name="message"
                       className="contact-form__textarea"
                       rows="9"
+                      onChange={handleChange}
+                      onBlur={validateMessage}
+                      value={message}
                     />
                   </label>
                 </p>
+                <div className='invalid-feedback'>{messageError}</div>
 
                 <div>
-                  <Button variant="orange" size="md" label="Lähetä" />
+                  <Button variant="orange" size="md" label="Lähetä" disabled={disable} />
                 </div>
               </>
             )}
