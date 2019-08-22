8428fb863e692d290c3569a3732530be7b912413
yazanAlhalabi <yazanalhalabi1@gmail.com>
2019-08-22 18:22:10 +0300

    default img to blog post

--- src/components/BlogPost/index.js
+++ src/components/BlogPost/index.js
@@ -3,13 +3,16 @@ import Img from "gatsby-image"
 import PropTypes from "prop-types"
 import { Link } from "gatsby"
 import classNames from "classnames"
-import defImage from "../../images/JANNE_HEIKKINEN_260619_77.jpg"
+import defImage from "../../assets/images/thumbnail.jpg"
 
-import { FacebookShareButton, TwitterShareButton, FacebookIcon,
-  TwitterIcon, } from "react-share"
+import {
+  FacebookShareButton,
+  TwitterShareButton,
+  FacebookIcon,
+  TwitterIcon,
+} from "react-share"
 
 import "./styles.scss"
-
 
 const BlogPost = ({
   isFluid,
@@ -20,35 +23,32 @@ const BlogPost = ({
   categories,
   tags,
   allSlugs,
-  slug
+  slug,
 }) => {
+  const shareUrl = "http://www.janneheikkinen.fi/blogi/" + slug
 
-  const shareUrl = "http://www.janneheikkinen.fi/blogi/" + slug;
+  const prevSlug = allSlugs[allSlugs.indexOf(slug) - 1]
+  const nextSlug = allSlugs[allSlugs.indexOf(slug) + 1]
+  let isFirst = false
+  let isLast = false
 
+  if (!image) image = defImage
 
+  if (!prevSlug) {
+    isFirst = true
+  }
 
-const prevSlug = allSlugs[allSlugs.indexOf(slug) - 1]
-const nextSlug = allSlugs[allSlugs.indexOf(slug) + 1]
-let isFirst = false;
-let isLast = false;
+  if (!nextSlug) {
+    isLast = true
+  }
 
-if (!image) image = defImage;
- 
-if (!prevSlug) {
-  isFirst = true;
-} 
+  const prevClasses = classNames({
+    prevDisplayNone: isFirst,
+  })
 
-if (!nextSlug) {
-  isLast = true;
-}
-
-const prevClasses = classNames({
-  prevDisplayNone: isFirst,
-})
-
-const nextClasses = classNames({
-  nextDisplayNone: isLast,
-})
+  const nextClasses = classNames({
+    nextDisplayNone: isLast,
+  })
 
   return (
     <div className="blog-post">
@@ -67,39 +67,41 @@ const nextClasses = classNames({
           <div className="blog-post__content__children">{children}</div>
         )}
 
-    <div className="blog-post__footer">
+        <div className="blog-post__footer">
           <div>
-        <div className="blog-post__categories">
-          Categories:<span>{categories}</span>
-        </div>
-        <div className="blog-post__tags">
-          Tags:<span>{tags}</span>
-        </div>
-        </div>
-        <div className="blog-post__share-button-container">
-        <FacebookShareButton
-            url={shareUrl}
-            quote={title}
-            className="blog-post__share-button">
-            <FacebookIcon
-              size={64}
-              round />
-          </FacebookShareButton>
+            <div className="blog-post__categories">
+              Categories:<span>{categories}</span>
+            </div>
+            <div className="blog-post__tags">
+              Tags:<span>{tags}</span>
+            </div>
+          </div>
+          <div className="blog-post__share-button-container">
+            <FacebookShareButton
+              url={shareUrl}
+              quote={title}
+              className="blog-post__share-button"
+            >
+              <FacebookIcon size={64} round />
+            </FacebookShareButton>
 
-          <TwitterShareButton
-            url={shareUrl}
-            quote={title}
-            className="blog-post__share-button">
-            <TwitterIcon
-              size={64}
-              round />
-          </TwitterShareButton>
+            <TwitterShareButton
+              url={shareUrl}
+              quote={title}
+              className="blog-post__share-button"
+            >
+              <TwitterIcon size={64} round />
+            </TwitterShareButton>
           </div>
-          </div>
+        </div>
       </div>
       <div className="blogPost-button-container">
-        <Link to={`blogi/${prevSlug}`}><button className={prevClasses}>◀ Edellinen</button></Link>
-        <Link to={`blogi/${nextSlug}`}><button className={nextClasses}>Seuraava ▶</button></Link>
+        <Link to={`blogi/${prevSlug}`}>
+          <button className={prevClasses}>◀ Edellinen</button>
+        </Link>
+        <Link to={`blogi/${nextSlug}`}>
+          <button className={nextClasses}>Seuraava ▶</button>
+        </Link>
       </div>
     </div>
   )
